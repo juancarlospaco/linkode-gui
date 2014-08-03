dcc627ed1e091aa9f8f6bc3438fb547aed26de2d
juan <juan.ojeda@globant.com>
2014-08-02 21:28:04 -0300

    Add short copyleft notice

--- linkode-gui.py
+++ linkode-gui.py
@@ -3,6 +3,20 @@
 # PEP8:OK, LINT:OK, PY3:OK
 
 
+#############################################################################
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 or 3.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following information to ensure GNU
+## General Public Licensing requirements will be met:
+## http:#www.fsf.org/licensing/licenses/info/GPLv2.html and
+## http:#www.gnu.org/copyleft/gpl.html.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#############################################################################
+
+
 # metadata
 """ Linkode GUI """
 __version__ = ' 0.0.1 '
@@ -213,6 +227,10 @@ class MainWindow(QMainWindow):
                 QInputDialog.getInt(self, __doc__, "Line number ?:", 1, 1)[0]
                 ) - 1, 0))
         editMenu.addSeparator()
+        editMenu.addAction("Count code lines", lambda: QMessageBox.information(
+            self, __doc__, str(len(
+                [a for a in self.code_editor.text().splitlines()
+                 if a.strip() != "" and not a.startswith("#")]))))
         editMenu.addAction("Force ignore modifications", lambda:
                            self.code_editor.setModified(False))
         sourceMenu = self.menuBar().addMenu("&Source")
